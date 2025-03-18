bcf37e4ea62a8c13b323e3e83dcfd16c2cc52552
Seba <122824272+SebaHel@users.noreply.github.com>
2025-03-18 21:32:03 +0100

    basic styling, created link navigation component

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import LinksNavigations from "@/components/LinksNavigations";
 
 export default function calendar() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: "#21252C",
       }}
     >
-      <Text>calendar screen.</Text>
+      <View style={styles.IconContainer}>
+        <LinksNavigations feathericonName={"user"} />
+        <LinksNavigations feathericonName={"settings"} />
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  IconContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    marginTop: 45,
+    margin: 25,
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
-
+import { Text, View, StyleSheet } from "react-native";
+import LinksNavigations from "@/components/LinksNavigations";
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: "#21252C",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={styles.IconContainer}>
+        <LinksNavigations feathericonName={"user"} />
+        <LinksNavigations feathericonName={"settings"} />
+      </View>
+      <Text>Welcome Back Name</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  IconContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    marginTop: 45,
+    margin: 25,
+  },
+});
