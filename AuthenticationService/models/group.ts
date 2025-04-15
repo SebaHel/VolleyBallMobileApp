6d8df037ef991fd945688d2cfeccc14f07d205c7
Seba <122824272+SebaHel@users.noreply.github.com>
2025-04-15 21:47:55 +0200

    feat: implemented isCoachofGroup

--- AuthenticationService/models/group.ts
+++ AuthenticationService/models/group.ts
@@ -39,3 +39,22 @@ export const addGroupMember = async (
     throw new Error("Query Bad Request");
   }
 };
+
+export const isCoachofGroup = async (
+  userId: string,
+  group_id: string
+): Promise<boolean> => {
+  try {
+    const query = `
+      SELECT user_id FROM group_members 
+      WHERE user_id = $1 AND group_id = $2 AND role = 'coach';
+    `;
+    const values = [userId, group_id];
+
+    const result = await pool.query(query, values);
+    return result.rowCount! > 0;
+  } catch (err: any) {
+    console.error("DB error (addGroupMember):", err.message);
+    throw new Error("Query Bad Request");
+  }
+};
